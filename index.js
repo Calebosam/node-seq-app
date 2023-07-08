fb0218e484a18f9520da76a276e9307d638f1e61
calebosam <caleb.osam@amalitech.com>
2023-07-08 17:25:49 +0000

    feat: setup seq for monitoring logs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const winston = require("winston");
 const { SeqTransport } = require("@datalust/winston-seq");
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(process.env.SEQ_URL);
+setTimeout(() => {
+  const logger = winston.createLogger({
+    transports: [
+      new SeqTransport({
+        serverUrl: process.env.SEQ_URL,
+        //   apiKey: "your-api-key",
+        onError: (e) => {
+          console.error(e);
+        },
+      }),
+    ],
+  });
 
-const logger = winston.createLogger({
-  transports: [
-    new SeqTransport({
-      serverUrl: "http://localhost:4607",
-      //   apiKey: "your-api-key",
-      onError: (e) => {
-        console.error(e);
-      },
-    }),
-  ],
-});
-
-logger.info("This is an event message");
+  logger.info("This is an event message");
+}, 5000);
